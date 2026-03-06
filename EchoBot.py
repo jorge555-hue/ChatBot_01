8531fc75600e3ba549fe08e02d9d2c806b7a4a7b
jorge555-hue <jorge55544@gmail.com>
2026-03-06 14:32:48 -0500

    Update EchoBot.py

--- EchoBot.py
+++ EchoBot.py
@@ -8,11 +8,44 @@ with st.chat_message("assistant"):
 with st.chat_message("Johnny"):
  st.write("Yes, would you like? 👋")
  st.bar_chart(np.random.randn(30, 3))
-'''
+
 import streamlit as st
 
 prompt = st.chat_input("Say something")
 if prompt:
  with st.chat_message("assistant"):
     st.write(f"User has sent the following prompt: {prompt}")
+'''
+    import streamlit as st
+
+    st.title("Echo Bot")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+
+    # React to user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        response = f"Echo: {prompt}"
+        # Display assistant response in chat message container
+        
+        with st.chat_message("assistant"):
+            st.markdown("Ok, continue ...")
+        
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+
 
