c63ae0bfb7a386943790a2d19d3cd43ff380895e
jorge555-hue <jorge55544@gmail.com>
2026-03-06 14:07:23 -0500

    Update EchoBot.py

--- EchoBot.py
+++ EchoBot.py
@@ -1,8 +1,10 @@
 
 import streamlit as st
+import numpy as np
 with st.chat_message("user"):
  st.write("Hello 👋")
 with st.chat_message("assistant"):
   st.write("Yes, would you like? 👋")
 with st.chat_message("Johnny"):
-  st.write("Yes, would you like? 👋")
+ st.write("Yes, would you like? 👋")
+ st.bar_chart(np.random.randn(30, 3))
