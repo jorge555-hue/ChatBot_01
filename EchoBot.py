b5b19ae263aa6e46b3aec8b6b1fd5d88a32abdb8
jorge555-hue <jorge55544@gmail.com>
2026-03-06 14:02:41 -0500

    Update EchoBot.py

--- EchoBot.py
+++ EchoBot.py
@@ -4,3 +4,5 @@ with st.chat_message("user"):
  st.write("Hello 👋")
 with st.chat_message("assistant"):
   st.write("Yes, would you like? 👋")
+ with st.chat_message("Johnny"):
+  st.write("Yes, would you like? 👋")
