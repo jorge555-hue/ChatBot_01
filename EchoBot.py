f3515754959decea2669fe9dd90df1c81f25ecf4
jorge555-hue <jorge55544@gmail.com>
2026-03-06 14:03:58 -0500

    Update EchoBot.py

--- EchoBot.py
+++ EchoBot.py
@@ -3,6 +3,6 @@ import streamlit as st
 with st.chat_message("user"):
  st.write("Hello 👋")
 with st.chat_message("assistant"):
-  st.write("Yes, would you like? 👋")w
+  st.write("Yes, would you like? 👋")
 with st.chat_message("Johnny"):
   st.write("Yes, would you like? 👋")
