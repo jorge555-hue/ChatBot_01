4375229b7b31373e1eec1e2a36730d5bd1de86a4
jorge555-hue <jorge55544@gmail.com>
2026-03-06 14:34:27 -0500

    Update EchoBot.py

--- EchoBot.py
+++ EchoBot.py
@@ -16,22 +16,22 @@ if prompt:
  with st.chat_message("assistant"):
     st.write(f"User has sent the following prompt: {prompt}")
 '''
-    import streamlit as st
+import streamlit as st
 
-    st.title("Echo Bot")
+st.title("Echo Bot")
 
     # Initialize chat history
-    if "messages" not in st.session_state:
+if "messages" not in st.session_state:
         st.session_state.messages = []
 
     # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
+ for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
 
     # React to user input
-    if prompt := st.chat_input("What is up?"):
+  if prompt := st.chat_input("What is up?"):
         # Display user message in chat message container
         st.chat_message("user").markdown(prompt)
 
